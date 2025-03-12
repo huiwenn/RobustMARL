6ece156e19eda01a3fe46c370b65ddb88aed2596
sophia <sophiaphia@live.com>
2025-03-12 05:57:22 +0000

    lines

--- multiagent/rendering.py
+++ multiagent/rendering.py
@@ -349,6 +349,7 @@ class PolyLine(Geom):
         self.close = close
         self.linewidth = LineWidth(1)
         self.add_attr(self.linewidth)
+        self.set_color(0, 0, 0, 0.5)  # Set alpha to 0.5 for transparency
 
     def render1(self):
         glBegin(GL_LINE_LOOP if self.close else GL_LINE_STRIP)
@@ -367,7 +368,6 @@ class Line(Geom):
         self.end = end
         self.linewidth = LineWidth(1)
         self.add_attr(self.linewidth)
-        self.set_color(0, 0, 0, 0.5)  # Set alpha to 0.5 for transparency
 
     def render1(self):
         glBegin(GL_LINES)
